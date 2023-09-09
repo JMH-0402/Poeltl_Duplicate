c33cd3eff7456c251c9bd6ac13f045056b9f9cee
Jeong Min Hwang <jeongmin.hwang@mail.utoronto.ca>
2023-09-09 18:52:54 -0400

    Created a new player finder function and updated the player generator function to return the height, weight, and position with the name, all in a list.

--- game_files/round.py
+++ game_files/round.py
@@ -1,5 +1,5 @@
 # This will be the class containing the rounds of each game
-from roster_files.random_player_generator import PlayerGenerator
+from roster_files.random_player_generator import *
 
 Num_of_players = 538
 
@@ -11,24 +11,59 @@ class Round:
     information based on their last guess. If the guessed NBA player plays in
     the same team, has the same height, or has the same weight, the player will
     be told that the correct component is green. if the guessed NBA player plays
-    in the same division, is within 3 inches of the correct player, or is within
+    in the same division, is within 2 inches of the correct player, or is within
     15 pounds, then they will be told that they are close to the answer with
     the close component being yellow
 
     === Attributes ===
     correct_player: The correct NBA player that the player tries to guess
     """
-    correct_player: str
+    correct_player: list
 
     def __init__(self):
         self.correct_player = PlayerGenerator()
 
-    def height_correctness(self, guess: str) -> bool:
+    def name_correctness(self, guess: str) -> bool:
         """
         Returns whether or not the guess is correct
-        :param guess:
-        :return:
+        :param guess: The guess that the player provides
+        :return: Whether or not the guess is correct or not
         """
+        if guess == self.correct_player:
+            return True
+        return False
+
+    def height_guess(self, guess: str) -> str:
+        """
+        Returns "correct" if the height of the guessed player is the same as the
+        correct_player. Returns "close" if the height of the guessed player is
+        2 inches off of the correct_player. Returns "incorrect" if the height of
+        the guessed player is off by more than 2 inches.
+        :param guess: The guess that the player provides
+        """
+        guess_player = PlayerFinder(guess)
+        if guess_player[2] == self.correct_player[2]:
+            return "correct"
+        elif abs(int(guess_player[2][2])-int(self.correct_player[2][2])) < 3:
+            return "close"
+        else:
+            return "incorrect"
+
+    def position_guess(self, guess: str) -> str:
+        """
+        Return "correct" if the position of the guessed player is the same as
+        the correct_player. Returns "close" if either the guessed player or the
+        correct player or both have two positions and they both have one
+        position in common. Returns "incorrect" if the position of
+        the guessed player is wrong.
+        :param guess: The name of the player
+        """
+        guess_player = PlayerFinder(guess)
+        if guess_player[1] == self.correct_player[1]:
+            return "correct"
+        elif len(guess_player[1]) > 1:
+
+
 
 if __name__ == '__main__':
     player = Round()
--- roster_files/random_player_generator.py
+++ roster_files/random_player_generator.py
@@ -4,20 +4,60 @@ import random
 import csv
 
 
-# This function will generate a random NBA player
-def PlayerGenerator() -> str:
+def PlayerGenerator() -> list:
+    """
+    Generates a random NBA player then returns the NBA player in a list where
+    the first index is the player name, second index is the player position,
+    third index is the players height, and the last index is the weight.
+    :return: The NBA player
+    """
+    player = []
     with open('player_names.csv', 'r') as file_obj:
 
         reader_obj = csv.DictReader(file_obj)
 
         n = random.randint(1, Constants.NUM_OF_PLAYERS)
-
         i = 1
         for row in reader_obj:
             if i == n:
-                return row['Player']
+                player.append(row["Player"])
             i += 1
+
+    for division in Constants.DIVISIONS:
+        for team in division:
+            with open('rosters_info/{}.csv'.format(team), 'r') as file_obj:
+                reader_obj = csv.DictReader(file_obj)
+
+                for row in reader_obj:
+                    if row["Player"] == player[0]:
+                        player.append(row["Pos"])
+                        player.append(row["Ht"])
+                        player.append(row["Wt"])
+
+    return player
+
+
+def PlayerFinder(player_name: str) -> list:
+    """
+    Returns the player_name's name, position, height, and weight in a list
+    :param player_name: the name of the player
+    :return: The players information
+    """
+    player = [player_name]
+    for division in Constants.DIVISIONS:
+        for team in division:
+            with open('rosters_info/{}.csv'.format(team), 'r') as file_obj:
+                reader_obj = csv.DictReader(file_obj)
+
+                for row in reader_obj:
+                    if row["Player"] == player_name:
+                        player.append(row["Pos"])
+                        player.append(row["Ht"])
+                        player.append(row["Wt"])
+
+    print(player)
+    return player
 
 
 if __name__ == '__main__':
-    PlayerGenerator()
+    PlayerFinder("Jeff Green")
